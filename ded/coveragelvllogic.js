2519b4805ae62e05bca74eac825c9d3d84f008f8
HippieHaley <haley.m.patino@gmail.com>
2025-08-11 22:51:01 +0000

    did why i split this

--- ded/coveragelvllogic.js
+++ ded/coveragelvllogic.js
@@ -1,4 +1,4 @@
-import Scale from './scale.js';
+import Scale from '../Project-2025/scale.js';
 const { coverageChart, awcChart } = Scale;
 
 const coverageLevels = ["0%", "25%", "50%", "75%", "100%"];
